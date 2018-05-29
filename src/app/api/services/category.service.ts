ef0c1e5c6e9c814888e06c53162df8d93e913101
Juan Hernández <juanhdezran@gmail.com>
2018-05-29 01:30:10 -0500

    bd actualizada completamente

--- src/app/api/services/category.service.ts
+++ src/app/api/services/category.service.ts
@@ -1,5 +1,12 @@
 export const CATEGORIES = [
     {id: 1, name: 'Tradicional (Poliestireno)'},
-    {id: 2, name: 'Tradicional Oro y Plata (Poliestireno)'},
-    {id: 3, name: 'Tradicional Oxford (Poliestireno)'}
+    {id: 2, name: 'ABC (Poliestireno)'},
+    {id: 3, name: 'Fantasy (Poliestireno)'},
+    {id: 4, name: 'Casino (Poliestireno)'},
+    {id: 5, name: 'Vintage (Poliestireno)'},
+    {id: 6, name: 'Bruselas (Poliestireno)'},
+    {id: 7, name: 'Metálicos (Poliestireno)'},
+    {id: 8, name: 'Crayons (Poliestireno)'},
+    {id: 9, name: 'Ecoframe (Poliestireno)'},
+    {id: 10, name: 'Gallery (Poliestireno)'}
 ];
